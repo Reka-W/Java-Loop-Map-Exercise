5c637c2c81915c689f1b98ca014ea109f80cbec2
reka_w <reka@rekacodes.com>
2023-04-29 00:58:15 -0400

    Completed classes and objects assignment

---
+++ src/ClassesAndObjects.java
@@ -0,0 +1,110 @@
+public class ClassesAndObjects {
+	
+	public static void main() {
+		
+	
+	}
+	
+	public class Pet {
+		private String name;
+		private int age;
+		private String location;
+		private String type;
+		
+		public String getName() {
+			return name;
+		}
+		
+		public void setName(String name) {
+			this.name = name;
+		}
+		
+		public int getAge() {
+			return age;
+		}
+		
+		public void setAge(int age) {
+			this.age = age;
+		}
+		
+		public void setLocation(String location) {
+			this.location = location;
+		}
+		
+		public String getType() {
+			return type;
+		}
+			
+		Pet() { }
+		
+		Pet(String name, int age, String location, String type) {
+			this.setName(name);
+			this.setAge(age);
+			this.setLocation(location); 
+			this.getType();
+		}
+	}
+	
+	public class Singer {
+		private String name;
+		private String firstAlbum;
+		private int yearBorn;
+		private String pronoun;
+		
+		public String getName() {
+			return name;
+		}
+		
+		public void setName(String name) {
+			this.name = name;
+		}
+		
+		public String getFirstAlbum() {
+			return firstAlbum;
+		}
+		
+		public void setFirstAlbum(String firstAlbum) {
+			this.firstAlbum = firstAlbum;
+		}
+		
+		public int getyearBorn() {
+			return yearBorn;
+		}
+		
+		public void setyearBorn(int yearBorn) {
+			this.yearBorn = yearBorn;
+		}
+		
+		public String getPronoun() {
+			return pronoun;
+		}
+		
+		public void setPronoun(String pronoun) {
+			this.pronoun = pronoun;
+		}
+		
+		Singer() { }
+		
+		Singer(String name, String firstAlbum) {
+			this.setName(name);
+			this.setFirstAlbum(firstAlbum);
+		}
+		
+		Singer(String name, String firstAlbum, int yearBorn, String pronoun) {
+			this.setName(name);
+			this.setFirstAlbum(firstAlbum);
+			this.setyearBorn(yearBorn);
+			this.setPronoun(pronoun);
+		}
+		
+		public void intro(String name, int yearBorn, String pronoun, String firstAlbum) {
+			System.out.println("Legendary singer " + name + " was born in " + yearBorn + ". " + pronoun + " first album was " + firstAlbum + ".");
+		}
+		
+		public void overFifty(int age) {
+			System.out.println(age >= 50);
+		}
+	}
+}
+
+
